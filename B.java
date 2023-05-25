2fe0ade51767ad3a5c8c932f962038cd8d4a4998
Laxman Moharana <lipu977moharana@gmail.com>
2023-05-26 00:25:50 +0530

    another java file also commited

--- B.java
+++ B.java
@@ -1,9 +1,12 @@
-class A {
+class B {
     static void method1(){
      System.out.println("My name is laxman kumar moharana");
 }
 static void method2(){
      System.out.println("i am from Baripada");
+}
+static void method3(){
+     System.out.println("i am complet my b-tech in computer science and engineering");
 }
 
 public static void main(String[] args){
