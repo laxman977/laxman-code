3f4ac4eb2f0818a46c78c73755d5465168bf5663
Laxman Moharana <lipu977moharana@gmail.com>
2023-05-26 00:31:49 +0530

    one more java fike which name is C

---
+++ C.java
@@ -0,0 +1,15 @@
+class C {
+    static void method1(){
+     System.out.println("My name is laxman kumar moharana");
+}
+static void method2(){
+     System.out.println("i am from Baripada");
+}
+static void method3(){
+     System.out.println("i am complet my b-tech in computer science and engineering");
+}
+
+public static void main(String[] args){
+method1();
+}
+}
